e748ed646c456d415e1e7f07b2d1be8800f3cb96
Lukas Waire <wairelm@gmail.com>
2017-06-24 01:00:49 -0500

    Major Changes cont.
    uploaded index.js

--- script/index.js
+++ script/index.js
@@ -1,17 +1,48 @@
 $(document).ready(function (){
+//Start Up
   $("#header").load("header.html");
   $("#content").load("indexfile.php");
   $("#footer").load("footer.php");
   $(".subMenu").hide();
   $("#home").addClass("active");
   $("#dthome").addClass("dtactive");
+  $("#xbutton").hide();
 
+// NAV MENU FOR DESKTOP AND MOBILE
+
+//X mobile button close function
+  function closeX(){
+//  $(".navSubButton").fadeOut("fast",function(){
+      $(".subMenu").slideUp("slow");
+  //  });
+  $("#xbutton").fadeOut("slow",function(){
+    $(".navDropButton").fadeIn("fast");
+  });
+}
+//Desktop Main Button function
+  function clickButton(){
+
+  }
+//Bars Mobile Button
   $(".navDropButton").on(("tap","click"), function(){
-    $(".subMenu").toggle();
+  //  $(".navSubButton").fadeOut("fast");
+    $(".subMenu").slideDown("slow",function(){
+    });
+    $(".navDropButton").fadeOut("fast",function(){
+      $("#xbutton").fadeIn("slow");
+    });
+  });
+//X Mobile Button
+  $("#xbutton").on(("tap","click"),function() {
+    closeX();
   });
 
+//Home Mobile Button
   $("#home").on(("tap","click"),function () {
-    $(".subMenu").hide();
+    if($("#xbutton").css("display","none")){
+    closeX();
+  }
+
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
     $(this).addClass("active");
@@ -20,7 +51,7 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Home Desktop Button
   $("#dthome").click(function() {
     $(".subMenu").hide();
     $("*").removeClass("active");
@@ -31,9 +62,11 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Contact Us Mobile Button
   $("#contactus").on(("tap","click"),function () {
-    $(".subMenu").hide();
+    if($("#xbutton").css("display","none")){
+    closeX();
+  }
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
     $(this).addClass("active");
@@ -44,7 +77,7 @@ $(document).ready(function (){
     $("#footer").hide();
     $(document).scrollTop(0);
   });
-
+//Contact Us Desktop Button
   $("#dtcontactus").click(function(){
     $(".subMenu").hide();
     $("*").removeClass("active");
@@ -57,9 +90,11 @@ $(document).ready(function (){
     $("#footer").hide();
     $(document).scrollTop(0);
   });
-
+//Activities Mobile Button
   $("#activities").on(("tap","click"),function () {
-    $(".subMenu").hide();
+    if($("#xbutton").css("display","none")){
+    closeX();
+  }
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
     $("#activities").addClass("active");
@@ -70,7 +105,7 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Activities Desktop Button
   $("#dtactivities").click(function () {
     $(".subMenu").hide();
     $("*").removeClass("active");
@@ -83,9 +118,11 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Curriculum Mobile Button
   $("#curriculum").on(("tap","click"),function () {
-    $(".subMenu").hide();
+    if($("#xbutton").css("display","none")){
+    closeX();
+  }
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
     $("#curriculum").addClass("active");
@@ -96,7 +133,7 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Curriculum Desktop Button
   $("#dtcurriculum").click(function () {
     $(".subMenu").hide();
     $("*").removeClass("active");
@@ -109,9 +146,11 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Our Facility Mobile Button
   $("#ourfacility").on(("tap","click"),function () {
-    $(".subMenu").hide();
+    if($("#xbutton").css("display","none")){
+    closeX();
+  }
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
     $("#ourfacility").addClass("active");
@@ -122,7 +161,7 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Our Facility Desktop Button
   $("#dtourfacility").click(function() {
     $(".subMenu").hide();
     $("*").removeClass("active");
@@ -135,9 +174,12 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//News Mobile Button
   $("#announcements").on(("tap","click"),function () {
-    $(".subMenu").hide();
+    if($("#xbutton").css("display","none")){
+    closeX();
+  }
+
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
     $("#announcements").addClass("active");
@@ -146,9 +188,10 @@ $(document).ready(function (){
     $("#dtannouncements").addClass("dtactive");
     $("#content").load("announcements.html");
     $("#footer").show();
+    closeX();
     $(document).scrollTop(0);
   });
-
+//News Desktop Button
   $("#dtannouncements").click(function() {
     $(".subMenu").hide();
     $("*").removeClass("active");
@@ -161,9 +204,11 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Registration & Rates Mobile Button
   $("#rates").on(("tap","click"), function () {
-    $(".subMenu").hide();
+    if($("#xbutton").css("display","none")){
+    closeX();
+  }
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
     $("#rates").addClass("active");
@@ -172,20 +217,22 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Registration & Rates Desktop Button
   $("#dtrates").click(function() {
     $(".subMenu").hide();
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
-    $("#rates").addClass("active");
+    $(this).addClass("active");
     $("#dtrates").addClass("dtactive");
     $("#content").load("rates.php");
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Pay/Payment Mobile Button
   $("#payment").on(("tap","click"),function () {
-    $(".subMenu").hide();
+    if($("#xbutton").css("display","none")){
+    closeX();
+  }
     $("*").removeClass("active");
     $("*").removeClass("dtactive");
     $("#payment").addClass("active");
@@ -194,7 +241,7 @@ $(document).ready(function (){
     $("#footer").show();
     $(document).scrollTop(0);
   });
-
+//Pay/Payment Desktop Button
   $("#dtpayment").click(function() {
     $(".subMenu").hide();
     $("*").removeClass("active");
